31dfdcee20006ec27b756b6c0c9d867bfe94cdce
Ryan Druffel <thefriendlynextdoorneighbor@gmail.com>
2018-09-04 13:32:21 -0400

    Express Skeleton
    I began writing the initial parts for a visualization system for the server but I kept getting a weird error, it may have to do with something in my firewall, last time I used express and port 7777, the port was unlocked but I doubt it is on my new laptop

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //Main code for LocatorApp node.js app
+const express = require('express');
+
+//This next portion is used to send information to the html file for visualization
+var app = express();
+
+test = 0;
+
+app.use(express.static(__dirname + '/'));//communicates with the HTML page
+app.listen(7777, function() { console.log('Node.JS is listening!'); });
+
+app.get('/test', function (req, res) {//sends test information
+	res.send(test);
+});
+
+function inc() {
+    test ++;
+}
+
+setInterval(inc, 5000);
